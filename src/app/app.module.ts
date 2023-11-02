fc5d4de7c2cddd92df38f729d19e0ef0d5eb940d
pshriya01 <shriya.shriya16@gmail.com>
2023-11-02 05:43:45 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoFormComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
